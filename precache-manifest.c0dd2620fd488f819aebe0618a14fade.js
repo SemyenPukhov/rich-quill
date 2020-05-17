31dc51b7a16f0fbcf195742171991e4dfc8cb586
Semyen <sspukhov@yandex.ru>
2020-05-17 23:54:29 +0300

    Updates

---
+++ precache-manifest.c0dd2620fd488f819aebe0618a14fade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b263395ffe13a4026723a874069c941",
+    "url": "/rich-quill/index.html"
+  },
+  {
+    "revision": "322bcc96c955dd380b81",
+    "url": "/rich-quill/static/css/main.c8864c1e.chunk.css"
+  },
+  {
+    "revision": "44bf8df0c61f78d92838",
+    "url": "/rich-quill/static/js/2.5428e614.chunk.js"
+  },
+  {
+    "revision": "b7a2a442e59b2fdd547fdc1e60520de1",
+    "url": "/rich-quill/static/js/2.5428e614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "322bcc96c955dd380b81",
+    "url": "/rich-quill/static/js/main.bc39c9ae.chunk.js"
+  },
+  {
+    "revision": "501595bcd12587e3d057",
+    "url": "/rich-quill/static/js/runtime-main.58c4d33a.js"
+  }
+]);
